620e90bfc8b4f7bcb7a5cca544335518b6af7e78
Benjamin Okoli <jaminokoli10@gmail.com>
2023-03-27 04:42:30 +0100

    Add reverse proxy script

--- compute/codedeploy_agent_install.sh
+++ compute/codedeploy_agent_install.sh
@@ -11,3 +11,30 @@ chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
 rm install
+
+#!/bin/bash
+
+# Install nginx
+sudo apt-get update
+sudo apt-get install nginx -y
+
+# Create a new configuration file for the reverse proxy
+sudo touch /etc/nginx/sites-available/reverse-proxy
+
+# Write the configuration to the file
+sudo echo "server {
+    listen 80;
+    server_name example.com;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+}" > /etc/nginx/sites-available/reverse-proxy
+
+# Create a symbolic link to enable the configuration
+sudo ln -s /etc/nginx/sites-available/reverse-proxy /etc/nginx/sites-enabled/
+
+# Restart nginx to apply the changes
+sudo systemctl restart nginx
